3836dc9222b4f728b88d8c16d7f2fd5448a96680
pasha <pashaganiev20@gmail.com>
2022-07-12 09:58:52 +0300

    testing version

---
+++ main.js
@@ -0,0 +1,6 @@
+$(".open").on('click', function() {
+    $(".custom-model-main").addClass('model-open');
+  }); 
+$(".close-btn, .bg-overlay").click(function(){
+    $(".custom-model-main").removeClass('model-open');
+});
